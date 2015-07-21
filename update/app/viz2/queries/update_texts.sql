037adf82565a144a4166b9e32673d6a5d3a32a50
Juan Elosua <juan.elosua@gmail.com>
2015-07-21 11:11:54 -0700

    Fix supQuemada on vis2

--- update/app/viz2/queries/update_texts.sql
+++ update/app/viz2/queries/update_texts.sql
@@ -11,7 +11,7 @@ FROM PIF2 p2
 WHERE p2.IDPIF = 2005450096;
 
 #Vis2 - Incendios por comunidad
-SELECT p1.IDCOMUNIDAD as idcomunidad, c.NOMBRE as comunidad, COUNT(*) as num_incendios, SUM(p9.totalar+p9.totalnar) as supquemada
+SELECT p1.IDCOMUNIDAD as idcomunidad, c.NOMBRE as comunidad, COUNT(*) as num_incendios, SUM(coalesce(p9.totalar,0)+coalesce(p9.totalnar,0)) as supquemada
 FROM PIF1 p1, PIF9 p9, COMUNIDADES c
 WHERE p9.IDPIF = p1.IDPIF
 AND p1.IDCOMUNIDAD = c.IDCOMUNIDAD
@@ -28,7 +28,7 @@ GROUP BY p1.IDCOMUNIDAD, c.NOMBRE, p0.ANIO
 
 #Vis2 - Data (Postgresql with required functions) total
 select count(*) as num_incendios, 
-SUM(p9."TOTALAR"+p9."TOTALNAR") as supquemada,
+SUM(coalesce(p9."TOTALAR",0)+coalesce(p9."TOTALNAR",0)) as supquemada,
 percentile_cont(array_agg(cast(EXTRACT(EPOCH FROM (p2."COMB_LLEGPMT" - p2."COMB_DET"))/60 as real)), 0.50) as median_llegpmt,
 percentile_cont(array_agg(cast(EXTRACT(EPOCH FROM (p2."COMB_CTRL" - p2."COMB_DET"))/60 as real)), 0.50) as median_ctrl,
 percentile_cont(array_agg(cast(EXTRACT(EPOCH FROM (p2."COMB_EXT" - p2."COMB_DET"))/60 as real)), 0.50) as median_ext
@@ -41,7 +41,7 @@ AND p2."COMB_EXT" is not NULL AND p2."COMB_CTRL" is not NULL and p2."COMB_LLEGPM
 #Vis2 - Data (Postgresql with required functions) by region
 select p1."IDCOMUNIDAD" as idcomunidad, trim(trailing from c."NOMBRE") as comunidad, 
 count(*) as num_incendios, 
-SUM(p9."TOTALAR"+p9."TOTALNAR") as supquemada,
+SUM(coalesce(p9."TOTALAR",0)+coalesce(p9."TOTALNAR",0)) as supquemada,
 percentile_cont(array_agg(cast(EXTRACT(EPOCH FROM (p2."COMB_LLEGPMT" - p2."COMB_DET"))/60 as real)), 0.50) as median_llegpmt,
 percentile_cont(array_agg(cast(EXTRACT(EPOCH FROM (p2."COMB_CTRL" - p2."COMB_DET"))/60 as real)), 0.50) as median_ctrl,
 percentile_cont(array_agg(cast(EXTRACT(EPOCH FROM (p2."COMB_EXT" - p2."COMB_DET"))/60 as real)), 0.50) as median_ext
@@ -50,6 +50,7 @@ WHERE p9."IDPIF" = p1."IDPIF"
 AND p9."IDPIF" = p2."IDPIF"
 AND p1."IDCOMUNIDAD" = c."IDCOMUNIDAD"
 AND p2."COMB_EXT" is not NULL AND p2."COMB_CTRL" is not NULL and p2."COMB_LLEGPMT" is not NULL
+AND p1."IDCOMUNIDAD" <> 18
 GROUP BY idcomunidad, comunidad
 ORDER BY idcomunidad;
 
@@ -57,7 +58,7 @@ ORDER BY idcomunidad;
 select p1."IDCOMUNIDAD" as idcomunidad, trim(trailing from c."NOMBRE") as comunidad,
 p0."ANIO" as anio, 
 count(*) as num_incendios, 
-SUM(p9."TOTALAR"+p9."TOTALNAR") as supquemada,
+SUM(coalesce(p9."TOTALAR",0)+coalesce(p9."TOTALNAR",0)) as supquemada,
 percentile_cont(array_agg(cast(EXTRACT(EPOCH FROM (p2."COMB_LLEGPMT" - p2."COMB_DET"))/60 as real)), 0.50) as median_llegpmt,
 percentile_cont(array_agg(cast(EXTRACT(EPOCH FROM (p2."COMB_CTRL" - p2."COMB_DET"))/60 as real)), 0.50) as median_ctrl,
 percentile_cont(array_agg(cast(EXTRACT(EPOCH FROM (p2."COMB_EXT" - p2."COMB_DET"))/60 as real)), 0.50) as median_ext
@@ -67,5 +68,6 @@ AND p9."IDPIF" = p1."IDPIF"
 AND p9."IDPIF" = p2."IDPIF"
 AND p1."IDCOMUNIDAD" = c."IDCOMUNIDAD"
 AND p2."COMB_EXT" is not NULL AND p2."COMB_CTRL" is not NULL and p2."COMB_LLEGPMT" is not NULL
+AND p1."IDCOMUNIDAD" <> 18
 GROUP BY idcomunidad, comunidad, anio
 ORDER BY idcomunidad, anio;
