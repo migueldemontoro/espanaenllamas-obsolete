31d336a28ae83c9faa4e864644aa409cb40a0de7
Juan Elosua <juan.elosua@gmail.com>
2015-07-21 11:11:34 -0700

    Fix supQuemada on vis1

--- update/app/viz1/queries/update_texts.sql
+++ update/app/viz1/queries/update_texts.sql
@@ -21,9 +21,9 @@ ORDER BY numIncendios desc;
 #Vis1 - Data
 SELECT p3.IDDETECTADOPOR, d.descripcion, p4.IDGRUPOCAUSA, g.descripcion,
 COUNT(*) AS numIncendios, COUNT(*) / T.total * 100 AS perc_count,
-SUM(p9.totalnar+p9.totalar) as supQuemada, SUM(p9.totalnar+p9.totalar) / T.supTotal * 100 AS perc_sup
+SUM(coalesce(p9.totalar,0)+coalesce(p9.totalnar,0)) as supQuemada, SUM(coalesce(p9.totalar,0)+coalesce(p9.totalnar,0)) / T.supTotal * 100 AS perc_sup
 FROM PIF3 p3, PIF4 p4, PIF9 p9, DETECTADOPOR d, GRUPOCAUSAS g,
-(SELECT COUNT(*) as total, SUM(p9.totalar+p9.totalnar) as supTotal FROM PIF9 p9) as T
+(SELECT COUNT(*) as total, SUM(coalesce(p9.totalar,0)+coalesce(p9.totalnar,0)) as supTotal FROM PIF9 p9) as T
 WHERE p9.IDPIF = p3.IDPIF
 AND p9.IDPIF = p4.IDPIF 
 AND p3.IDDETECTADOPOR = d.IDDETECTADOPOR
